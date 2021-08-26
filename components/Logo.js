cfb99ac8d62ab62613961ba296d7bf0eaa7cb81c
Vincent Hirtz <vincent.hirtz@sapiendo.fr>
2021-08-26 17:51:08 +0200

    change currency and image product

--- components/Logo.js
+++ components/Logo.js
@@ -4,9 +4,9 @@ import Image from 'next/image';
 const Logo = () => (
   <Link href="/">
     <a className="flex items-center space-x-2">
-      <Image src="/leaf.svg" alt="Logo" width={32} height={32} />
+      <Image src="/logo.jpg" alt="Logo" width={50} height={50} />
       <span className="hidden sm:inline-block font-extrabold text-3xl text-gray-700">
-        MyPlantShop
+        Petit boukan
       </span>
     </a>
   </Link>
