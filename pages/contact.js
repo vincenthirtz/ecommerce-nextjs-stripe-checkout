bf523f42163139b420a9f11a666ce87263eeb1e3
Vincent Hirtz <hirtzvincent@gmail.com>
2021-09-09 09:24:26 +0200

    update seo for contact and boutike

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,18 @@
+import Head from 'next/head';
 
 export default function Contact() {
-
   return (
-    <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        Contact
+    <>
+      <Head>
+        <title>Contact | Petit Boukan | France</title>
+        <meta name="description" content="Page de contact de Petit Boukan" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container xl:max-w-screen-xl mx-auto py-12 px-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          Contact
+        </div>
       </div>
-    </div>
+    </>
   );
 }
